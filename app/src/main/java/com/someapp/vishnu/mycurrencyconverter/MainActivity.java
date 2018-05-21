4c421d8cd70428414bf1c95b8112de169c0020b3
Vishnu Adda <addavishnu@gmail.com>
2018-05-20 19:40:06 -0700

    Added code to the onClick method which will convert pounds to dollars.

--- app/src/main/java/com/someapp/vishnu/mycurrencyconverter/MainActivity.java
+++ app/src/main/java/com/someapp/vishnu/mycurrencyconverter/MainActivity.java
@@ -3,11 +3,19 @@ package com.someapp.vishnu.mycurrencyconverter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view) {
-        //yes
+        EditText edit = (EditText) findViewById(R.id.amountEntered);
+
+        String poundString = edit.getText().toString();
+        double poundValue = Double.parseDouble(poundString);
+        Double dollarValue = poundValue*1.35;
+
+        Toast.makeText(this, "£" + poundString + " is $" + dollarValue.toString(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
