ffa52ca11ac3a286bbb5dae40896fc144be194db
Vishnu Adda <addavishnu@gmail.com>
2018-05-20 19:29:56 -0700

    test

---
+++ app/src/main/java/com/someapp/vishnu/mycurrencyconverter/MainActivity.java
@@ -0,0 +1,18 @@
+package com.someapp.vishnu.mycurrencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void onClick(View view) {
+        //yes
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
