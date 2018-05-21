19ce2b7e782fbcc9555299a6fa69155cc590982e
Vishnu Adda <addavishnu@gmail.com>
2018-05-20 20:12:50 -0700

    Some conditions made for improper cases like non-numeric values or no values at all. Also added method activity to log.

--- app/src/main/java/com/someapp/vishnu/mycurrencyconverter/MainActivity.java
+++ app/src/main/java/com/someapp/vishnu/mycurrencyconverter/MainActivity.java
@@ -2,6 +2,7 @@ package com.someapp.vishnu.mycurrencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -9,10 +10,29 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view) {
-        EditText edit = (EditText) findViewById(R.id.amountEntered);
 
+        Log.i("Info", "Button Pressed");
+
+        EditText edit = findViewById(R.id.amountEntered);
         String poundString = edit.getText().toString();
-        double poundValue = Double.parseDouble(poundString);
+
+        if(poundString.length() == 0) {
+            Toast.makeText(this, "Enter a number value before pressing the button",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        for(int i=0; i< poundString.length(); i++) {
+            if(!Character.isDigit(poundString.charAt(i))) {
+                Toast.makeText(this,
+                        "Only enter number values", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        Log.i("Pounds", poundString);
+
+        Double poundValue = Double.parseDouble(poundString);
         Double dollarValue = poundValue*1.35;
 
         Toast.makeText(this, "£" + poundString + " is $" + dollarValue.toString(), Toast.LENGTH_SHORT).show();
